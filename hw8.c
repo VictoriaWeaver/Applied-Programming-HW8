e6bf531fea145b2ebdf2b2a19613a02a2da32658
Victoria Weaver <victoriaweaver@Victorias-MacBook-Air.local>
2015-11-28 19:37:07 -0500

    Added usage checks to hw8.c

--- hw8.c
+++ hw8.c
@@ -4,3 +4,35 @@
 *
 * Homework 8, Driver program for cubic splines implementations.
 *************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "interp.h"
+#include "Dynamic.h"
+
+
+int main(int argc, char *argv[]){
+	/* Check if proper amount of commandline arguments */
+	if(1 >= argc){
+
+		/* Check for first 3 usage cases */
+		if((3 == argc) && ((0 == strcmp(argv[1], "-nak")) ||  (0 == strcmp(argv[1], "-nat")) || (0 == strcmp(argv[1], "-cl")))){
+
+		}
+		/* Check for -e use case */
+		else if((4 == argc) && (0 == strcmp(argv[1],"-e"))){
+
+		}
+		else{
+			/* Print usage message */
+			fprintf(stdout, "Usage: ./hw8 <-nak, -nat, -cl> data.txt \nUsage: ./hw8 -e sparameters.txt eval.txt\n");	
+		}
+
+	}
+	else{
+		/* Print usage message */
+		fprintf(stdout, "Usage: ./hw8 <-nak, -nat, -cl> data.txt \nUsage: ./hw8 -e sparameters.txt eval.txt\n");
+	}
+}
