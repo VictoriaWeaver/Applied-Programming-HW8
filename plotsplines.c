4df3cd790e0a8e07cac5e5b25ddbed5b346bc3fa
Victoria Weaver <victoriaweaver@Victorias-MacBook-Air.local>
2015-12-02 16:28:52 -0500

    modifying plotsplines to get the natural and not-a-knot splines on one plot: needs some sort of distinguishing factor in png

--- plotsplines.c
+++ plotsplines.c
@@ -9,7 +9,7 @@
  Warning : popen is not part of standard C; expect warnings if using -ansi !
 
  To view the png figure use "display" (do not forget to set your X11 terminal first)
- ***********************************************************************************/
+ ***************************************************************************/
 #include <stdio.h>
 
 int main(void) 
@@ -35,7 +35,7 @@ int main(void)
 #endif
   /* gnuplot: send output to png file with desired font */
   fprintf(pipe,"set terminal png enhanced font 'Vera.ttf'\n"); 
-  fprintf(pipe,"set output 'splines1.png' \n"); 
+  fprintf(pipe,"set output 'splines.png' \n"); 
 
   /* gnuplot: set title, labels, legend  and line styles */
   fprintf(pipe,"set title 'Cubic spline interpolation result '\n");
@@ -44,32 +44,7 @@ int main(void)
   fprintf(pipe,"set style data lines\n"); /* draw lines between data pts  */
   fprintf(pipe,"set xrange [-2:5]\n");		/* set x range		  */ 
   /* read data file with (x,y) points */
-  fprintf(pipe,"plot 'plotPoints.txt' using 1:2 lt 1 lw 2 t 'cubic spline' \n");
-
-  /*************************************/
-  fprintf(pipe,"set output 'splines2.png' \n");
-
-  /* gnuplot: set title, labels, legend  and line styles */
-  fprintf(pipe,"set title 'Cubic spline interpolation result '\n");
-  fprintf(pipe,"set xlabel 'x'\n");
-  fprintf(pipe,"set ylabel 'f(x)'\n");
-  fprintf(pipe,"set style data lines\n"); /* draw lines between data pts  */
-  fprintf(pipe,"set xrange [-2:5]\n");          /* set x range            */
-  /* read data file with (x,y) points */
-  fprintf(pipe,"plot 'plotPoints.txt' using 3:4 lt 1 lw 2 t 'cubic spline' \n");
-
-  /*************************************/
-  fprintf(pipe,"set output 'splines3.png' \n");
-
-  /* gnuplot: set title, labels, legend  and line styles */
-  fprintf(pipe,"set title 'Cubic spline interpolation result '\n");
-  fprintf(pipe,"set xlabel 'x'\n");
-  fprintf(pipe,"set ylabel 'f(x)'\n");
-  fprintf(pipe,"set style data lines\n"); /* draw lines between data pts  */
-  fprintf(pipe,"set xrange [-2:5]\n");          /* set x range            */
-  /* read data file with (x,y) points */
-  fprintf(pipe,"plot 'plotPoints.txt' using 5:6 lt 1 lw 2 t 'cubic spline' \n");
-
+  fprintf(pipe,"plot 'plotPoints.txt' using 1:2 lt 1 lw 2 t 'cubic spline nat','plotPoints.txt' using 3:4 lt 1 lw 2 t 'cubic spline nak' \n");
 
 /* Close pipe */
   pclose(pipe);
